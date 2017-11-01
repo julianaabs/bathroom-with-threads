6b57af18841baaaf26416fef1ecbfcb3d954690d
Nalbert Gabriel <nalbertg@outlook.com>
2017-11-01 17:40:56 -0300

    first

---
+++ src/monitor/Human.java
@@ -0,0 +1,21 @@
+package monitor;
+
+public class Human {
+	private char gender; // 'M' == Male or 'F' to female
+	private int timeToFinish;
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	char getGender() {
+		return this.gender;
+	}
+
+	public void run() {
+		// pede para entrar no banheiro
+		// enviar ao banheiro mensagem dizendo que entrou
+		// de forma randomica gerar quanto tempo vai levar no banheiro
+		// enviar mensagem ao banheiro dizendo que saiu
+	}
+}
